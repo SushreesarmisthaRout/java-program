b5fe03d298241f8f13e101b334ad9a0a725a2150
SushreesarmisthaRout <sushreesarmistha320@gmail.com>
2025-02-19 15:52:15 +0530

    Add files via upload

---
+++ JAVA_CODES/dx.java
@@ -0,0 +1,31 @@
+public class dx {
+
+    public static void main(String[] args) {
+        String su = "Editor";
+        System.out.println(su);
+    }
+}
+
+// import java.util.Scanner;
+
+// public class dx {
+// public static void jx() {
+// Scanner sc = new Scanner(System.in);
+// System.out.print("Enter the words: ");
+// String input = sc.nextLine();
+// int count = 0;
+
+// try {
+// String[] words = input.split("\\s+");
+// count = words.length;
+// System.out.println("Number of words: " + count);
+// } catch (Exception e) {
+// System.out.println("Error.");
+// }
+// sc.close();
+// }
+
+// public static void main(String[] args) {
+// jx();
+// }
+// }
